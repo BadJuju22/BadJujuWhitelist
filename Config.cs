202f233d81997446b3594c097581e40639b3f0b5
BadJuJu <79955157+BadJuju22@users.noreply.github.com>
2022-01-15 20:23:31 +0700

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,20 @@
+﻿
+using Rocket.API;
+using SDG.Unturned;
+using System.Collections.Generic;
+
+namespace BadJujuWhiteList
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public string whitelistperm;
+        public int timetokick;
+        public void LoadDefaults()
+        {
+            whitelistperm = "pass";
+            timetokick = 30;
+            
+           
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,73 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core;
+using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Linq;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+
+namespace BadJujuWhiteList
+{
+    public class Plugin : RocketPlugin<Config>
+    {
+
+        public override TranslationList DefaultTranslations => new TranslationList
+        {
+                 { "message_kick", "Вы не находитесь в вайт листе" },
+        };
+
+        public static Plugin Instance { get; private set; }
+        public string text2;
+        protected override void Load()
+        {
+            Instance = this;
+
+            U.Events.OnBeforePlayerConnected += Events_OnPlayerConnected;
+        }
+
+       
+
+        private void Events_OnPlayerConnected(UnturnedPlayer uPlayer)
+        {
+            
+          
+            
+            if (uPlayer.HasPermission(Configuration.Instance.whitelistperm))
+            {
+
+            }
+            else
+            {
+                KickPlayer(uPlayer);
+            }
+
+        }
+        private async Task KickPlayer(UnturnedPlayer uPlayer)
+        {
+            await Task.Delay(Configuration.Instance.timetokick*1000);
+            if (uPlayer.HasPermission(Configuration.Instance.whitelistperm))
+            {
+
+            }
+            else
+            {
+                uPlayer.Kick(Plugin.Instance.Translate("message_kick"));
+            }
+        }
+
+        protected override void Unload()
+        {
+
+            U.Events.OnBeforePlayerConnected -= Events_OnPlayerConnected;
+        }
+    }
+}
+
+  
+
